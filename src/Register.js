da6009518693a76e4a21ca526a3f4071cd9b0a87
itbeauregard <i.t.beauregard@gmail.com>
2017-10-02 14:09:40 -0400

    Initial commit
    Starter code came from a tutorial I found on a Medium blog post:
    https://medium.com/technoetics/create-basic-login-forms-using-create-react-app-module-in-reactjs-511b9790dede
    
    I began altering the code to be more specific to my Rails back end API
    which has slightly different parameters to receive. I am currently having
    an issue fetching data where the axios call appears to be making an
    OPTIONS call to the sign-up route, instead of POST.

---
+++ src/Register.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import axios from 'axios';
+import Login from './Login';
+
+class Register extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      name: '',
+      username:'',
+      password:''
+    }
+  }
+
+  handleClick(event) {
+    var apiBaseUrl = "http://localhost:4741";
+    console.log("values", this.state.name, this.state.username, this.state.password);
+    //To be done:check for empty values before hitting submit
+    var self = this;
+    var payload = {
+      "credentials": {
+        "name": this.state.name,
+        "username": this.state.username,
+        "password": this.state.password
+      }
+    }
+    axios.post(apiBaseUrl + '/sign-up', payload)
+      .then(function(response) {
+          console.log(response);
+          if (response.data.code == 200) {
+            //  console.log("registration successfull");
+            var loginscreen = [];
+            loginscreen.push( < Login parentContext = {
+                this
+              }
+              />);
+              var loginmessage = "Not Registered yet.Go to registration"; self.props.parentContext.setState({
+                loginscreen: loginscreen,
+                loginmessage: loginmessage,
+                buttonLabel: "Register",
+                isLogin: true
+              });
+            }
+          })
+        .catch(function(error) {
+          console.log(error);
+        });
+
+  }
+
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider>
+          <div>
+          <AppBar
+             title="Register"
+           />
+           <TextField
+             hintText="Enter your Name"
+             floatingLabelText="Name"
+             onChange = {(event,newValue) => this.setState({name:newValue})}
+             />
+           <br/>
+           <TextField
+             hintText="Enter your Username"
+             floatingLabelText="Username"
+             onChange = {(event,newValue) => this.setState({username:newValue})}
+             />
+           <br/>
+           <TextField
+             type="password"
+             hintText="Enter your Password"
+             floatingLabelText="Password"
+             onChange = {(event,newValue) => this.setState({password:newValue})}
+             />
+           <br/>
+           <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+          </div>
+         </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+const style = {
+  margin: 15,
+};
+export default Register;
---
+++ src/UploadScreen.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class UploadScreen extends Component {
+
+  render() {
+    return (
+        <div>
+          <h1>YEAH!</h1>
+        </div>
+    )
+  }
+}
+
+export default UploadScreen;
