cd854cb95abb6870d120f437057a87a7dda73a2a
itbeauregard <i.t.beauregard@gmail.com>
2017-10-02 22:42:42 -0400

    Add clickable image cards to the uploadscreen component
    Doesn't look like my wireframe yet but I'm enjoying seeing what Material
    can do. Right now, the clicks merely trigger console.logs but the goal is
    to add a little click animation and transition to another component when a
    card is clicked.

--- src/UploadScreen.js
+++ src/UploadScreen.js
@@ -4,6 +4,8 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+// import Tab from 'material-ui/Tabs';
 
 class UploadScreen extends Component {
   constructor(props) {
@@ -11,12 +13,33 @@ class UploadScreen extends Component {
     this.state = {
       open: false
     }
+    this.handleToggle = this.handleToggle.bind(this)
+    this.handleFoxPoint = this.handleFoxPoint.bind(this)
+    this.handleDowntown = this.handleDowntown.bind(this)
+    this.handleEastSide = this.handleEastSide.bind(this)
+    this.handleSmithHill = this.handleSmithHill.bind(this)
   }
 
   handleToggle() {
     this.setState({
       open: !this.state.open
     })
+  }
+
+  handleFoxPoint() {
+    console.log("You clicked me!")
+  }
+
+  handleDowntown() {
+    console.log("You went downtown!")
+  }
+
+  handleEastSide() {
+    console.log("You went east!")
+  }
+
+  handleSmithHill() {
+    console.log("You went up dah hill!")
   }
 
   render() {
@@ -33,6 +56,27 @@ class UploadScreen extends Component {
                </Drawer>
              }
            />
+           <h2>Choose your quest:</h2>
+           <Card onClick={this.handleFoxPoint}>
+            <CardMedia overlay={<CardTitle title="Fox Point" />}>
+              <img src="https://i.imgur.com/oydon46.jpg" alt='fox point'/>
+            </CardMedia>
+           </Card>
+           <Card onClick={this.handleDowntown}>
+            <CardMedia overlay={<CardTitle title="Downtown" />}>
+              <img src="https://i.imgur.com/xKbUw25.jpg" alt='downtown'/>
+            </CardMedia>
+           </Card>
+           <Card onClick={this.handleEastSide}>
+            <CardMedia overlay={<CardTitle title="East Side" />}>
+              <img src="https://i.imgur.com/YiUbUVN.jpg" alt='east side'/>
+            </CardMedia>
+           </Card>
+           <Card onClick={this.handleSmithHill}>
+            <CardMedia overlay={<CardTitle title="Smith Hill" />}>
+              <img src="https://i.imgur.com/NIVcddj.jpg" alt='smith hill'/>
+            </CardMedia>
+           </Card>
         </MuiThemeProvider>
       </div>
     )
