3e35502f3af11355876b9c2ec3d55436f5c9645a
itbeauregard <i.t.beauregard@gmail.com>
2017-10-02 21:17:24 -0400

    Add home screen component
    Launches after successful log in.
    
    Future concerns: Do I need to re-write
    the appbar for every component/page? Seems redundant and not ideal.

--- src/UploadScreen.js
+++ src/UploadScreen.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 
 class UploadScreen extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: false
+    }
+  }
+
+  handleToggle() {
+    this.setState({
+      open: !this.state.open
+    })
+  }
 
   render() {
     return (
-        <div>
-          <h1>YEAH!</h1>
-        </div>
+      <div>
+        <MuiThemeProvider>
+          <AppBar
+             title="Home"
+             onClick={this.handleToggle}
+             children={
+               <Drawer open={this.state.open}>
+                 <MenuItem>Profile</MenuItem>
+                 <MenuItem>Sign Out</MenuItem>
+               </Drawer>
+             }
+           />
+        </MuiThemeProvider>
+      </div>
     )
   }
 }
