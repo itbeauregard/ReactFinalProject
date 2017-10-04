889979d9548edd38be4eee6f0445bcc0e69db2d8
itbeauregard <i.t.beauregard@gmail.com>
2017-10-04 15:25:57 -0400

    Move QuestionScreen code into Choices component
    This component will be useful because there will be multiple nestings going
    on in this component because each location has multiple questions and
    each question has multiple choices.

--- src/QuestionScreen.js
+++ src/QuestionScreen.js
@@ -3,60 +3,18 @@ import { NavBar } from './NavBar';
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
+import { Choices } from './Choices'
 
 export class QuestionScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      answer: ''
-    }
-  }
-
   render() {
     return (
       <div>
-        <MuiThemeProvider>
-          <NavBar
-            token={this.props.token}
-            id={this.props.id}
-          />
-          <Card>
-            <CardHeader
-              title={this.props.question}
-              actAsExpander={true}
-              showExpandableButton={true} />
-            <CardText expandable={true}>
-              <RadioButtonGroup>
-                <RadioButtonGroup
-                  value={this.props.choices[0]}
-                  label={this.props.choices[0]}
-                />
-                <RadioButtonGroup
-                  value={this.props.choices[1]}
-                  label={this.props.choices[1]}
-                />
-                <RadioButtonGroup
-                  value={this.props.choices[2]}
-                  label={this.props.choices[2]}
-                />
-                <RadioButtonGroup
-                  value={this.props.choices[3]}
-                  label={this.props.choices[3]}
-                />
-                <RadioButtonGroup
-                  value={this.props.choices[4]}
-                  label={this.props.choices[4]}
-                />
-              </RadioButtonGroup>
-            </CardText>
-          </Card>
-          <Card>
-            <CardHeader
-              title={this.props.question}
-              actAsExpander={true}
-              showExpandableButton={true} />
-          </Card>
-        </MuiThemeProvider>
+        <Choices
+          question={this.props.question}
+          choices={this.props.choices}
+          token={this.props.token}
+          id={this.props.idea}
+        />
       </div>
     )
   }
