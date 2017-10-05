8d56359798fb5c28a10a8bddf5f1f0c58193c952
itbeauregard <i.t.beauregard@gmail.com>
2017-10-04 22:09:24 -0400

    Refactor Choices component
    Choices component now merely draws the choices of a question

--- src/Choices.js
+++ src/Choices.js
@@ -12,43 +12,14 @@ export class Choices extends Component {
   render() {
     return(
       <div>
-        <MuiThemeProvider>
-          <NavBar
-            token={this.props.token}
-            id={this.props.id}
-          />
-          <Card>
-            <CardHeader
-              title={this.props.question}
-              actAsExpander={true}
-              showExpandableButton={true} />
-
-            <CardText expandable={true}>
-              <RadioButtonGroup name='choices'>
-                <RadioButtonGroup
-                  value={this.props.choices[0]}
-                  label={this.props.choices[0]}
-                />
-                <RadioButtonGroup
-                  value={this.props.choices[1]}
-                  label={this.props.choices[1]}
-                />
-                <RadioButtonGroup
-                  value={this.props.choices[2]}
-                  label={this.props.choices[2]}
-                />
-                <RadioButtonGroup
-                  value={this.props.choices[3]}
-                  label={this.props.choices[3]}
-                />
-                <RadioButtonGroup
-                  value={this.props.choices[4]}
-                  label={this.props.choices[4]}
-                />
-              </RadioButtonGroup>
-            </CardText>
-          </Card>
-        </MuiThemeProvider>
+        <RadioButtonGroup name='choices'>
+            {this.props.choices.map(function(each) {
+              return (<RadioButtonGroup
+                        value={each}
+                        label={each}
+                        />)
+            })}
+        </RadioButtonGroup>
       </div>
     )
   }
