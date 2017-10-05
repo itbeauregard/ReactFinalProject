9b6144a11883e02b359f6825e5f3edf5ee366a63
itbeauregard <i.t.beauregard@gmail.com>
2017-10-05 11:48:30 -0400

    Create entry in user_answers table with each complete answer
    Add button disabler for when correct answer is selected.

--- src/Choices.js
+++ src/Choices.js
@@ -3,12 +3,16 @@ import { NavBar } from './NavBar';
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
+import { host } from './server';
+import axios from 'axios';
+
 
 export class Choices extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      usermessage: ''
+      usermessage: '',
+      disable: false
     }
 
     this.correctClick = this.correctClick.bind(this);
@@ -16,19 +20,36 @@ export class Choices extends Component {
   }
 
   correctClick() {
+    // disable buttons after correct is selected
     this.setState({
-      usermessage: 'Correct!'
+      usermessage: 'Correct!',
+      disable: true
     })
-    console.log(this.state.usermessage)
+    // TODO add user answer table update
+    // needed: user id (comes from credentials), question id (the initial get all call),
+    // token (from credentials)
+    axios({
+      method: 'post',
+      url: host + '/user_answers',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token token=' + this.props.credentials.state.token
+      },
+      data: {
+        'user_answer': {
+          'user_id': this.props.credentials.state.id,
+          'question_id': this.props.question_id,
+          'complete': true
+        }
+      }
+    })
   }
 
   wrongClick() {
     this.setState({
       usermessage: 'Try again.'
     })
-    console.log(this.state.usermessage)
   }
-
 
   render() {
     let self = this;
@@ -37,15 +58,17 @@ export class Choices extends Component {
         <RadioButtonGroup name='choices'>
             {this.props.choices.map(function(each) {
               // TODO: if the choice is not correct, style red and say try again
-              // if choice is correct, style green and say correct!
+              // if choice is correct, style green and say correct
               if (each === self.props.correct) {
                 return (<RadioButton
+                          disabled={self.state.disable}
                           value={each}
                           label={each}
                           onClick={self.correctClick}
                           />)
               } else {
                 return (<RadioButton
+                          disabled={self.state.disable}
                           value={each}
                           label={each}
                           onClick={self.wrongClick}
