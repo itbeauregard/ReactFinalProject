b3777f25b3f13acc646e8c7c082c909b2be844bb
itbeauregard <i.t.beauregard@gmail.com>
2017-10-05 10:46:02 -0400

    Alter radio buttons to include an answer validation system
    Now each button gets its own clickhandler based on whether it is the
    correct answer. To do: submit completions to the user_answer table.

--- src/Choices.js
+++ src/Choices.js
@@ -7,19 +7,53 @@ import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
 export class Choices extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      usermessage: ''
+    }
+
+    this.correctClick = this.correctClick.bind(this);
+    this.wrongClick = this.wrongClick.bind(this);
   }
 
+  correctClick() {
+    this.setState({
+      usermessage: 'Correct!'
+    })
+    console.log(this.state.usermessage)
+  }
+
+  wrongClick() {
+    this.setState({
+      usermessage: 'Try again.'
+    })
+    console.log(this.state.usermessage)
+  }
+
+
   render() {
+    let self = this;
     return(
       <div>
         <RadioButtonGroup name='choices'>
             {this.props.choices.map(function(each) {
-              return (<RadioButtonGroup
-                        value={each}
-                        label={each}
-                        />)
+              // TODO: if the choice is not correct, style red and say try again
+              // if choice is correct, style green and say correct!
+              if (each === self.props.correct) {
+                return (<RadioButton
+                          value={each}
+                          label={each}
+                          onClick={self.correctClick}
+                          />)
+              } else {
+                return (<RadioButton
+                          value={each}
+                          label={each}
+                          onClick={self.wrongClick}
+                          />)
+              }
             })}
         </RadioButtonGroup>
+        <p>{this.state.usermessage}</p>
       </div>
     )
   }
