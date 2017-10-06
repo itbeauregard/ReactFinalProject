59451ee350d1f7beb8488d8bb6693d95d21c61bf
itbeauregard <i.t.beauregard@gmail.com>
2017-10-05 21:49:05 -0400

    Indent block of code

--- src/TableData.js
+++ src/TableData.js
@@ -24,16 +24,16 @@ export class TableData extends Component {
           {self.props.data.questions.map(function(each) {
             return (
               <TableRow>
-              <TableRowColumn>{each.id}</TableRowColumn>
-              <TableRowColumn>{each.location}</TableRowColumn>
-              <TableRowColumn>{each.ask}</TableRowColumn>
-              <TableRowColumn>{each.first_choice}</TableRowColumn>
-              <TableRowColumn>{each.second_choice}</TableRowColumn>
-              <TableRowColumn>{each.third_choice}</TableRowColumn>
-              <TableRowColumn>{each.fourth_choice}</TableRowColumn>
-              <TableRowColumn>{each.fifth_choice}</TableRowColumn>
-              <TableRowColumn>{each.correct_choice}</TableRowColumn>
-            </TableRow>
+                <TableRowColumn>{each.id}</TableRowColumn>
+                <TableRowColumn>{each.location}</TableRowColumn>
+                <TableRowColumn>{each.ask}</TableRowColumn>
+                <TableRowColumn>{each.first_choice}</TableRowColumn>
+                <TableRowColumn>{each.second_choice}</TableRowColumn>
+                <TableRowColumn>{each.third_choice}</TableRowColumn>
+                <TableRowColumn>{each.fourth_choice}</TableRowColumn>
+                <TableRowColumn>{each.fifth_choice}</TableRowColumn>
+                <TableRowColumn>{each.correct_choice}</TableRowColumn>
+              </TableRow>
             )
           })}
       </MuiThemeProvider>
