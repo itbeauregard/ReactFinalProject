dd836c86a05fcd85cc2a0584163e4ce82ed0c990
itbeauregard <i.t.beauregard@gmail.com>
2017-10-05 20:13:09 -0400

    Create AdminButtons component
    This component is made to be inserted on all Admin screens so that the
    functions are available to the user on all screens.

---
+++ src/AdminButtons.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import { QuestionTableScreen } from './QuestionTableScreen';
+import { QuestionCreator } from './QuestionCreator';
+import axios from 'axios';
+import { host } from './server';
+
+export class AdminButtons extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: false
+    }
+    this.getAllQuestionsClick = this.getAllQuestionsClick.bind(this);
+    this.createQuestionClick = this.createQuestionClick.bind(this);
+  }
+
+  getAllQuestionsClick() {
+    let self = this;
+    axios({
+      url: host + '/questions',
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token token=' + this.props.credentials.state.token
+      }
+    })
+    .then(function(response) {
+      const data = response.data;
+      self.props.appContext.setState({
+        uploadScreen: <QuestionTableScreen
+                        data={data}
+                        appContext={self.props.appContext}
+                        credentials={self.props.credentials}
+                      />
+      })
+    })
+    .catch(function(err) {
+      console.error(err)
+    })
+  }
+
+  createQuestionClick() {
+    this.props.appContext.setState({
+      uploadScreen: <QuestionCreator
+                      appContext={this.props.appContext}
+                      credentials={this.props.credentials}
+                    />
+    })
+  }
+
+  // TODO: Have a menu with create, update, delete, and get action options
+  render() {
+    const style = { margin: 12 }
+    return (
+      <div>
+        <MuiThemeProvider>
+          <RaisedButton
+            label="See all Questions"
+            secondary={true}
+            style={style}
+            onClick={this.getAllQuestionsClick}
+          />
+          <RaisedButton
+            label="Create Question"
+            primary={true}
+            style={style}
+            onClick={this.createQuestionClick}
+          />
+        </MuiThemeProvider>
+      </div>
+    )
+  }
+}
