8602e89311b1c7cc9577948c7fb707092b9bfd63
itbeauregard <i.t.beauregard@gmail.com>
2017-10-05 20:14:12 -0400

    Move the buttons to AdminButtons component

--- src/AdminScreen.js
+++ src/AdminScreen.js
@@ -2,55 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import { NavBar } from './NavBar'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { QuestionCreator } from './QuestionCreator';
-import RaisedButton from 'material-ui/RaisedButton';
-import { QuestionTableScreen } from './QuestionTableScreen';
-import axios from 'axios';
-import { host } from './server';
+import { AdminButtons } from './AdminButtons';
 
 export class AdminScreen extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      open: false
-    }
-    this.getAllQuestionsClick = this.getAllQuestionsClick.bind(this);
-    this.createQuestionClick = this.createQuestionClick.bind(this);
-  }
-
-  getAllQuestionsClick() {
-    let self = this;
-    axios({
-      url: host + '/questions',
-      method: 'get',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Token token=' + this.props.credentials.state.token
-      }
-    })
-    .then(function(response) {
-      const data = response.data;
-      self.props.appContext.setState({
-        uploadScreen: <QuestionTableScreen
-                        data={data}
-                        appContext={self.props.appContext}
-                        credentials={self.props.credentials}
-                      />
-      })
-    })
-    .catch(function(err) {
-      console.error(err)
-    })
-  }
-
-  createQuestionClick() {
-    this.props.appContext.setState({
-      uploadScreen: <QuestionCreator
-                      appContext={this.props.appContext}
-                      credentials={this.props.credentials}
-                    />
-    })
-  }
 
   // TODO: Have a menu with create, update, delete, and get action options
   render() {
@@ -62,17 +16,9 @@ export class AdminScreen extends Component {
             appContext={this.props.appContext}
             credentials={this.props.credentials}
           />
-          <RaisedButton
-            label="See all Questions"
-            secondary={true}
-            style={style}
-            onClick={this.getAllQuestionsClick}
-          />
-          <RaisedButton
-            label="Create Question"
-            primary={true}
-            style={style}
-            onClick={this.createQuestionClick}
+          <AdminButtons
+            appContext={this.props.appContext}
+            credentials={this.props.credentials}
           />
         </MuiThemeProvider>
       </div>
